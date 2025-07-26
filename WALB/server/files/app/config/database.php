29d975eb6a35a2dd018bfd9933c88aa787d3ae96
Ggoon23 <xogoon@naver.com>
2025-07-26 16:31:22 +0900

    mysql to postgresql

--- WALB/server/files/app/config/database.php
+++ WALB/server/files/app/config/database.php
@@ -61,8 +61,6 @@ try {
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
-        PDO::MYSQL_ATTR_SSL_CA => null,
-        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
         PDO::ATTR_TIMEOUT => 10,
         PDO::ATTR_PERSISTENT => false
     ];
